63d69f3c4b7d72aafcdce0c593a5e2389201ff9f
KHJcode <to@khjcode.com>
2022-11-30 17:43:59 +0900

    Update player, person

--- src/v1/player/Person.java
+++ src/v1/player/Person.java
@@ -1,7 +1,31 @@
 package v1.player;
 
+import java.util.Scanner;
+
 public class Person extends Player {
     public Person(String nickname) {
         super(nickname);
+    }
+
+    // 범위 내의 숫자를 입력 받고 반환
+    public int inputNumberWithinRange(char axis, int min, int max) {
+        Scanner input = new Scanner(System.in);
+        int number;
+        while (true) {
+            System.out.println("입력 " + axis + " (" + min + " ~ " + max + ") :");
+            number = input.nextInt();
+            if (number >= min && number <= max) {
+                break;
+            }
+            System.out.println("시스템: 범위 내의 숫자를 입력해주세요!");
+        }
+        return number;
+    }
+
+    @Override
+    public void handleTurn() {
+        int x, y;
+        x = this.inputNumberWithinRange('X', 1, 8);
+        y = this.inputNumberWithinRange('Y', 1, 8);
     }
 }
--- src/v1/player/Player.java
+++ src/v1/player/Player.java
@@ -1,6 +1,6 @@
 package v1.player;
 
-public class Player {
+public abstract class Player {
     private final String nickname;
 
     Player(String nickname) {
@@ -8,11 +8,16 @@ public class Player {
         this.handleJoin();
     }
 
+    // 플레이어의 닉네임을 반환
     public String getNickname() {
         return this.nickname;
     }
 
+    // 플레이어가 게임에 입장했을 때 제어
     public void handleJoin() {
         System.out.println("Player " + this.nickname + " is joined!");
     }
+
+    // 플레이어의 차례가 왔을 때 제어
+    abstract public void handleTurn();
 }
