5b6c733cea43c9fc8f79890bff1537a7f1a8f84b
KHJcode <to@khjcode.com>
2022-12-01 11:14:03 +0900

    Update person for rules

--- src/v1/player/Person.java
+++ src/v1/player/Person.java
@@ -3,8 +3,9 @@ package v1.player;
 import v1.Game;
 import v1.piece.Piece;
 
-import java.io.Console;
 import java.util.Scanner;
+
+import static v1.Common.WHITE_ID;
 
 public class Person extends Player {
     private Game game;
@@ -37,16 +38,23 @@ public class Person extends Player {
     public void handleTurn() {
         boolean result = false;
         while (!result) {
-            int currentX = this.inputNumberWithinRange("선택", 'X', 1, 8) - 1;
-            int currentY = this.inputNumberWithinRange("선택", 'Y', 1, 8) - 1;
-            if (this.game.getBoard().getPiece(currentX, currentY) == null) {
-                System.out.println("시스템: 선택하신 칸은 현재 비어있습니다!");
+            int currentX = this.inputNumberWithinRange("선택", 'X', 1, 8);
+            int currentY = this.inputNumberWithinRange("선택", 'Y', 1, 8);
+            Piece selectedPiece = this.game.getBoard().getPiece(currentX - 1, currentY - 1);
+            if (selectedPiece == null) {
+                this.game.handleUpdateBoard();
+                System.out.println("시스템: 선택하신 칸은 현재 비어있습니다! " + "(" + currentX + "," + currentY + ")");
+            }
+            else if (selectedPiece.getTeamId() != WHITE_ID) {
+                this.game.handleUpdateBoard();
+                System.out.println("시스템: 선택하신 칸의 말은 다른 팀입니다! " + "(" + currentX + "," + currentY + ")");
             } else {
-                int targetX = this.inputNumberWithinRange("이동", 'X', 1, 8) - 1;
-                int targetY = this.inputNumberWithinRange("이동", 'Y', 1, 8) - 1;
-                result = this.game.put(currentX, currentY, targetX, targetY);
+                int targetX = this.inputNumberWithinRange("이동", 'X', 1, 8);
+                int targetY = this.inputNumberWithinRange("이동", 'Y', 1, 8);
+                result = this.game.put(currentX - 1, currentY - 1, targetX - 1, targetY - 1);
                 if (!result) {
-                    System.out.println("시스템: 규칙에 따라 착수할 수 없습니다!");
+                    this.game.handleUpdateBoard();
+                    System.out.println("시스템: 규칙에 따라 착수할 수 없습니다! " + "(" + targetX + "," + targetY + ")");
                 }
             }
         }
