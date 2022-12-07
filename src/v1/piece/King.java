2af0cedded0db9a25c86a3be5fa12012f8849031
KHJcode <to@khjcode.com>
2022-12-07 14:26:35 +0900

    Update king

--- src/v1/piece/King.java
+++ src/v1/piece/King.java
@@ -2,7 +2,6 @@ package v1.piece;
 
 import v1.Board;
 
-import static v1.Common.jul;
 import static v1.Common.pieceTeams;
 
 public class King extends Piece {
@@ -10,16 +9,84 @@ public class King extends Piece {
         super(teamId, PieceId.KING.get(), pieceTeams.get(teamId).getKing());
     }
 
-
-    @Override
     public boolean getCanMove(Board board, int nowX, int nowY, int moveX, int moveY, int targetStatus) {
-        return false;
-        /*
         if (targetStatus == 1) {
             return false;
         } else {
-            return jul(nowX - moveX) >= 0 && jul(nowY - moveY) > 0 && !((nowY - moveY) == 0 && (nowX - moveX) == (0));
+            if (nowX < moveX && nowY < moveY) {
+                int i = nowX + 1;
+                int j = nowY + 1;
+                while (i < moveX && j < moveY) {
+                    if (board.getPiece(i, j) != null)
+                        return false;
+                    i++;
+                    j++;
+                }
+                return true;
+            } else if (nowX < moveX && nowY > moveY) {
+                int i = nowX + 1;
+                int j = nowY - 1;
+                while (i < moveX && j > moveY) {
+                    if (board.getPiece(i, j) != null)
+                        return false;
+                    i++;
+                    j--;
+                }
+                return true;
+            } else if (nowX > moveX && nowY < moveY) {
+                int i = nowX - 1;
+                int j = nowY + 1;
+                while (i > moveX && j < moveY) {
+
+                    if (board.getPiece(i, j) != null)
+                        return false;
+                    i--;
+                    j++;
+                }
+                return true;
+            } else if (nowX > moveX && nowY > moveY) {
+
+                int i = nowX - 1;
+                int j = nowY - 1;
+                while (i > moveX && j > moveY) {
+
+                    if (board.getPiece(i, j) != null)
+                        return false;
+                    i--;
+                    j--;
+                }
+                return true;
+            } else if (nowX != moveX) {
+                if (nowX < moveX) {
+                    for (int i = nowX + 1; i < moveX; i++) {
+                        if (board.getPiece(i, moveY) != null)
+                            return false;
+
+                    }
+                } else {
+                    for (int i = moveX + 1; i < nowX; i++) {
+                        if (board.getPiece(i, moveY) != null)
+                            return false;
+
+                    }
+                }
+            } else {
+                if (nowY < moveY) {
+                    for (int i = nowY + 1; i < moveY; i++) {
+                        if (board.getPiece(moveX, i) != null)
+                            return false;
+
+                    }
+                } else {
+                    for (int i = moveY + 1; i < nowY; i++) {
+                        if (board.getPiece(moveX, i) != null)
+                            return false;
+
+                    }
+                }
+            }
+
         }
-        */
+        return true;
     }
 }
